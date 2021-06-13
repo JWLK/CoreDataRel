567c7638c681e515b25a330c85e92a69b7dd653e
JWLK <utxtion@me.com>
2021-06-14 02:55:59 +0900

    Initial Commit

---
+++ CoreDataRel/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoreDataRel
+//
+//  Created by JWLK on 2021/06/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CoreDataRel/CoreDataRelApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataRelApp.swift
+//  CoreDataRel
+//
+//  Created by JWLK on 2021/06/14.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataRelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
